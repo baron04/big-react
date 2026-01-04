86c2aebe037704ded7bfc662374c8d0191f351ad
baron04 <lcc6048@gmail.com>
2026-01-04 15:23:24 +0800

    8-1 实现hooks架构

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,6 +1,20 @@
+import currentDispatcher, {
+	Dispatcher,
+	resolveDispatcher
+} from './src/currentDispatcher';
 import { jsxDEV } from './src/jsx';
 
 export default {
 	version: '0.0.0',
 	createElement: jsxDEV
+};
+
+export const useState: Dispatcher['useState'] = (initialState) => {
+	const dispatcher = resolveDispatcher();
+	return dispatcher.useState(initialState);
+};
+
+// 内部数据共享层
+export const __SECRET_INTERNAL_NO_NOT_USE_OR_YOU_WILL_BE_FIRED = {
+	currentDispatcher
 };
