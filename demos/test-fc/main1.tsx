606241f0fe1f16ad833028f79d4f817ad8b50a9b
baron04 <lcc6048@gmail.com>
2026-01-06 22:23:30 +0800

    11 实现事件系统

--- demos/test-fc/main1.tsx
+++ demos/test-fc/main1.tsx
@@ -7,71 +7,15 @@ import { createRoot } from 'react-dom/client';
 
 // console.log(import.meta.hot);
 
-const jsx = (
-	<div>
-		<span>big react</span>
-	</div>
-);
-
 function Child() {
+	return 'child';
+}
+
+function App() {
 	const [num, setNum] = useState(100);
 	window.setNum = setNum;
 
-	const arr =
-		num % 2 == 0
-			? [<li key="1">1</li>, <li key="2">2</li>, <li key="3">3</li>]
-			: [<li key="3">3</li>, <li key="2">2</li>, <li key="1">1</li>];
-	return (
-		<div>
-			{/* {num == 3 ? (
-				<div>{jsx}</div>
-			) : (
-				<span onClickCapture={() => setNum(num + 1)}>{num}</span>
-			)} */}
-			{/* <span onClick={() => setNum(num + 1)}>{num}</span> */}
-
-			<span
-				onClick={() => {
-					setNum((num) => num + 1);
-					setNum((num) => num + 1);
-					setNum((num) => num + 1);
-				}}
-			>
-				{num}
-			</span>
-			<ul>{arr}</ul>
-		</div>
-	);
+	return <div>{num == 3 ? <Child /> : <div>{num}</div>}</div>;
 }
 
-const fragmentTest1 = (
-	<>
-		<div></div>
-		<div></div>
-	</>
-);
-
-const fragmentTest2 = (
-	<ul>
-		<>
-			<li>1</li>
-			<li>2</li>
-		</>
-		<li>3</li>
-		<li>4</li>
-	</ul>
-);
-
-function App() {
-	return (
-		<div>
-			<Child />
-			{/* {fragmentTest2} */}
-		</div>
-	);
-}
-
-// console.log(React);
-console.log(jsx);
-
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')!).render(<App />);
---
+++ packages/react-dom/src/SyntheticEvent.ts
@@ -0,0 +1,127 @@
+import { Container } from 'hostConfig';
+import { Props } from 'shared/ReactTypes';
+
+export const elementPropsKey = '__props';
+
+const validEventTypeList = ['click'];
+
+type EventCallback = (e: Event) => void;
+
+interface SyntheticEvent extends Event {
+	__stopProgation: boolean;
+}
+
+interface Paths {
+	capture: EventCallback[];
+	bubble: EventCallback[];
+}
+
+export interface DOMElement extends Element {
+	[elementPropsKey]: Props;
+}
+
+export function updateFiberProps(node: DOMElement, props: Props) {
+	node[elementPropsKey] = props;
+}
+
+export function initEvent(container: Container, eventType: string) {
+	if (!validEventTypeList.includes(eventType)) {
+		console.warn(`当前不支持${eventType}事件`);
+		return;
+	}
+
+	if (__DEV__) {
+		console.log(`初始化事件: ${eventType}`);
+	}
+
+	container.addEventListener(eventType, (event) => {
+		dispatchEvent(container, eventType, event);
+	});
+}
+
+function createSyntheticEvent(event: Event) {
+	const syntheticEvent = event as SyntheticEvent;
+	syntheticEvent.__stopProgation = false;
+	const originStopPropagation = event.stopPropagation;
+	syntheticEvent.stopPropagation = () => {
+		syntheticEvent.__stopProgation = true;
+		if (originStopPropagation) {
+			originStopPropagation.call(event);
+		}
+	};
+	return syntheticEvent;
+}
+
+function dispatchEvent(container: Container, eventType: string, event: Event) {
+	const targetElement = event.target;
+
+	if (targetElement === null) {
+		console.warn('事件不存在target', event);
+		return;
+	}
+	// 1. 收集沿途事件
+	const { bubble, capture } = collectPaths(
+		targetElement as DOMElement,
+		container,
+		eventType
+	);
+	// 2. 构造合成事件
+	const se = createSyntheticEvent(event);
+	// 3. 遍历capture
+	triggerEventFlow(capture, se);
+	if (!se.__stopProgation) {
+		// 4. 遍历bubble
+		triggerEventFlow(bubble, se);
+	}
+}
+
+function triggerEventFlow(paths: EventCallback[], se: SyntheticEvent) {
+	for (let i = 0; i < paths.length; i++) {
+		const callback = paths[i];
+		callback.call(null, se);
+
+		if (se.__stopProgation) {
+			break;
+		}
+	}
+}
+
+function getEventCallbackNameFromEventType(
+	eventType: string
+): string[] | undefined {
+	return {
+		click: ['onClickCapture', 'onClick']
+	}[eventType];
+}
+
+function collectPaths(
+	targetElement: DOMElement,
+	container: Container,
+	eventType: string
+) {
+	const paths: Paths = {
+		capture: [],
+		bubble: []
+	};
+
+	while (targetElement && targetElement !== container) {
+		// 收集
+		const elementProps = targetElement[elementPropsKey];
+		if (elementProps) {
+			const callbackNameList = getEventCallbackNameFromEventType(eventType);
+			callbackNameList?.forEach((callbackName, index) => {
+				const eventCallback = elementProps[callbackName];
+				if (eventCallback) {
+					if (index === 0) {
+						paths.capture.unshift(eventCallback);
+					} else {
+						paths.bubble.push(eventCallback);
+					}
+				}
+			});
+		}
+		targetElement = targetElement.parentNode as DOMElement;
+	}
+
+	return paths;
+}
